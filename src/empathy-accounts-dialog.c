d439f6812b1bcd6375ddd25dafddf7a2e1d70ae7
Daniel Boles <dboles@src.gnome.org>
2016-12-01 03:25:30 +0000

    Trivial cleanups to recent commits
     * roster-window.ui: Fix indentation of the added line
     * accounts-dialog: Move border-width from .c to .ui, since when we
       have a .ui file, it seems better to keep the declarative stuff there.

--- src/empathy-accounts-dialog.c
+++ src/empathy-accounts-dialog.c
@@ -2401,7 +2401,6 @@ accounts_dialog_build_ui (EmpathyAccountsDialog *dialog)
   gtk_window_set_role (GTK_WINDOW (dialog), "accounts");
   gtk_window_set_default_size (GTK_WINDOW (dialog), 640, 450);
   gtk_window_set_type_hint (GTK_WINDOW (dialog), GDK_WINDOW_TYPE_HINT_DIALOG);
-  gtk_container_set_border_width (GTK_CONTAINER (dialog), 12);
 
   /* join the add/remove toolbar to the treeview */
   context = gtk_widget_get_style_context (sw);
