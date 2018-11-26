ee939e4c6e9ad92bb8ed400f65997f557cf5050b
Ferdinand Stehle <development@kondorgulasch.de>
2018-11-26 21:22:06 +0100

    show web view errors in log

--- libempathy-gtk/empathy-theme-adium.c
+++ libempathy-gtk/empathy-theme-adium.c
@@ -509,6 +509,43 @@ nsdate_to_strftime (EmpathyAdiumData *data, const gchar *nsdate)
 }
 
 static void
+web_view_javascript_finished (GObject      *object,
+                              GAsyncResult *result,
+                              gpointer      user_data)
+{
+    WebKitJavascriptResult *js_result;
+    JSValueRef              value;
+    JSGlobalContextRef      context;
+    GError                 *error = NULL;
+
+    js_result = webkit_web_view_run_javascript_finish (WEBKIT_WEB_VIEW (object), result, &error);
+    if (!js_result) {
+        g_warning ("Error running javascript: %s", error->message);
+        g_error_free (error);
+        return;
+    }
+
+    context = webkit_javascript_result_get_global_context (js_result);
+    value = webkit_javascript_result_get_value (js_result);
+    if (JSValueIsString (context, value)) {
+        JSStringRef js_str_value;
+        gchar      *str_value;
+        gsize       str_length;
+
+        js_str_value = JSValueToStringCopy (context, value, NULL);
+        str_length = JSStringGetMaximumUTF8CStringSize (js_str_value);
+        str_value = (gchar *)g_malloc (str_length);
+        JSStringGetUTF8CString (js_str_value, str_value, str_length);
+        JSStringRelease (js_str_value);
+        g_print ("Script result: %s\n", str_value);
+        g_free (str_value);
+    } else {
+        g_warning ("Error running javascript: unexpected return value");
+    }
+    webkit_javascript_result_unref (js_result);
+}
+
+static void
 theme_adium_add_html (EmpathyThemeAdium *self,
     const gchar *func,
     const gchar *html,
@@ -728,7 +765,7 @@ theme_adium_add_html (EmpathyThemeAdium *self,
     }
 
   script = g_string_free (string, FALSE);
-  webkit_web_view_run_javascript (WEBKIT_WEB_VIEW (self), script, NULL, NULL, NULL);
+  webkit_web_view_run_javascript (WEBKIT_WEB_VIEW (self), script, NULL, web_view_javascript_finished, NULL);
   g_free (script);
 }
 
